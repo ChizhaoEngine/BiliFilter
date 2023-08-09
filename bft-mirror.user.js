a0afabc9fc36b011a58d0781423b88a541cccabf
池沼动力 <114285377+ChizhaoEngine@users.noreply.github.com>
2023-08-09 13:43:02 +0800

    .

--- bft-mirror.user.js
+++ bft-mirror.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliFilter3(mirror)
 // @namespace    https://github.com/ChizhaoEngine/BiliFilter
-// @version      0.3.11.230809
+// @version      0.3.11
 // @description  杀掉你不想看到的东西
 // @author       池沼动力
 // @license      CC BY-NC-ND 4.0
