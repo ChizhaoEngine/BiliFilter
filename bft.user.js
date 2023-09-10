3e1158cbc822afbb1b599db41597ab7ded37f72d
池沼动力 <114285377+ChizhaoEngine@users.noreply.github.com>
2023-09-10 22:38:51 +0800

    Update bft.user.js

--- bft.user.js
+++ bft.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliFilter3
 // @namespace    https://github.com/ChizhaoEngine/BiliFilter
-// @version      0.3.13.3
+// @version      0.3.13.4
 // @description  杀掉你不想看到的东西
 // @author       池沼动力
 // @license      CC BY-NC-ND 4.0
@@ -819,12 +819,14 @@
 
     `);
     // 当浏览器关闭时,将面板标记为关闭
-    window.addEventListener('beforeunload', () => {
+    window.addEventListener('beforeunload', (e) => {
         // 只有当本页面有设置面板打开时才需要
         if (document.getElementById('bft-menu')) {
             // 添加已关闭面板的标记
             GM_setValue("temp_isMenuOpen", false);
         }
+        console.log(e)
+
     });
 
 
@@ -2506,6 +2508,7 @@
             document.getElementById('bft-copyright').innerHTML = GM_info.script.copyright;
             document.getElementById('bft-close-window').addEventListener("click", function () {
                 document.getElementById('bft-aboutDialog').remove();
+                GM_setValue("temp_isMenuOpen", false);
             });
 
         }
